8d4dd06f9c1150f182f8afe3a7c2a4da8c94aa74
mangix <maqh1988@gmail.com>
2014-12-14 16:20:08 +0800

    bin

--- bin/nbalive.js
+++ bin/nbalive.js
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
-var args = process.argv;
-var date = args[2];
+var program = require('commander');
+var util = require("../lib/util");
+
+var defaultDate = util.format(new Date());
+
+program
+    .version('1.0.0')
+    .option('-d, --date [date]', 'choose date', checkDate)
+    .parse(process.argv);
 
 var live = require("../lib/live");
 
-live("hupu", date);
+live("hupu", program.date || defaultDate);
+
+
+function checkDate(aDate) {
+    if (/^\d{4}[-\/]\d{1,2}[-\/]\d{1,2}/.test(aDate)) {
+        return util.format(new Date(aDate));
+    } else {
+        return defaultDate;
+    }
+
+}
