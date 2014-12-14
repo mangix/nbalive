d7a79c6f42f4843ec5436148d99150aff369af2d
mangix <maqh1988@gmail.com>
2014-12-14 16:21:21 +0800

    bin and package info

--- bin/nbalive.js
+++ bin/nbalive.js
@@ -5,7 +5,7 @@ var util = require("../lib/util");
 var defaultDate = util.format(new Date());
 
 program
-    .version('1.0.0')
+    .version('0.1.0')
     .option('-d, --date [date]', 'choose date', checkDate)
     .parse(process.argv);
 
